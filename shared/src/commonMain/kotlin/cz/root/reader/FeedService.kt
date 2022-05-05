6de82993bf8ec4a2959de89a147b92b3d157373d
Jakub Vano <jakub.vano@cleverlance.com>
2022-05-05 17:49:58 +0200

    Load rss feed

--- shared/src/commonMain/kotlin/cz/root/reader/FeedService.kt
+++ shared/src/commonMain/kotlin/cz/root/reader/FeedService.kt
@@ -1,26 +1,37 @@
 package cz.root.reader
 
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsText
+import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.serializer
+import nl.adaptivity.xmlutil.serialization.UnknownChildHandler
+import nl.adaptivity.xmlutil.serialization.XML
+
 class FeedService {
+    private val httpClient = HttpClient(httpEngine)
+    private val xml = XML(SerializersModule {}) {
+        unknownChildHandler = UnknownChildHandler { _,_,_,_,_ -> emptyList() }
+    }
 
     suspend fun loadItems(): List<FeedItem> {
-        return listOf(
-            FeedItem(
-                id = FeedItem.Id("1"),
-                title = "Feed Item 1",
-                description = "Description of the feed item number one",
-                author = "Author",
-                web = null,
-                image = Image("https://picsum.photos/id/1/200"),
-            ),
-            FeedItem(
-                id = FeedItem.Id("1"),
-                title = "Feed Item 1",
-                description = "Description of the feed item number one",
-                author = "Author",
-                web = null,
-                image = Image("https://picsum.photos/id/2/200"),
-            ),
-        )
+        val xmlString = httpClient.get(RSS_URL).bodyAsText()
+        val rssDto = xml.decodeFromString(serializer<RssDto>(), xmlString)
+        return rssDto.channel.items.map(::toDomain)
+    }
+
+    private fun toDomain(dto: ItemDto) = FeedItem(
+        id = FeedItem.Id(dto.guid),
+        title = dto.title,
+        description = dto.description,
+        author = dto.author,
+        web = dto.link?.let(::Web),
+        image = dto.enclosure?.url?.let(::Image)
+    )
+
+    companion object {
+        const val RSS_URL = "https://www.root.cz/rss/clanky"
     }
 }
 
